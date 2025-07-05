7f8710a2d80271013f2906272afe1f91e8948086
xiaoyu <166600771+zeyu8023@users.noreply.github.com>
2025-07-05 14:52:08 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app.jsx';
+import App from './App';
 import './index.css';
 import { getRuntimeConfig } from './config';
 import { initApi } from './lib/api';
-import React from 'react';
 
 getRuntimeConfig().then((config) => {
   window.API_BASE = config.VITE_API_BASE;
-  initApi(); // 初始化 axios 实例
+  initApi();
 
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
+  ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
