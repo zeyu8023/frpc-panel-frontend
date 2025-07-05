fee46a9327f0b8c0219174b4d18344e24f39a68d
xiaoyu <166600771+zeyu8023@users.noreply.github.com>
2025-07-05 15:23:34 +0800

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,7 @@ import Proxies from './pages/Proxies';
 import Logs from './pages/Logs';
 import Account from './pages/Account';
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 import React from 'react';
 
 function App() {
@@ -15,11 +16,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/proxies" element={<Proxies />} />
-          <Route path="/logs" element={<Logs />} />
-          <Route path="/account" element={<Account />} />
+          <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/settings" element={<PrivateRoute><Settings /></PrivateRoute>} />
+          <Route path="/proxies" element={<PrivateRoute><Proxies /></PrivateRoute>} />
+          <Route path="/logs" element={<PrivateRoute><Logs /></PrivateRoute>} />
+          <Route path="/account" element={<PrivateRoute><Account /></PrivateRoute>} />
         </Routes>
       </div>
     </Router>
