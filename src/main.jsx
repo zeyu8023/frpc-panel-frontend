e0ca90ad7e8e52bb29e91ce9f38da950cdd03515
xiaoyu <166600771+zeyu8023@users.noreply.github.com>
2025-07-05 13:59:41 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app.jsx';
+import './index.css';
 import { getRuntimeConfig } from './config';
 
 getRuntimeConfig().then((config) => {
   window.API_BASE = config.VITE_API_BASE;
 
-  import('./bootstrap').then(({ mountApp }) => {
-    mountApp();
-  });
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 });
