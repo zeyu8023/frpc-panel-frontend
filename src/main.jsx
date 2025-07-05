0901f68be9b19c32d44922192ea1410ccc7c2e36
xiaoyu <166600771+zeyu8023@users.noreply.github.com>
2025-07-05 14:01:40 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './app.jsx';
 import './index.css';
 import { getRuntimeConfig } from './config';
+import { initApi } from './lib/api';
 
 getRuntimeConfig().then((config) => {
   window.API_BASE = config.VITE_API_BASE;
+  initApi(); // 初始化 axios 实例
 
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
