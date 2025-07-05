b5813e16e3a8f0a38b85f6ce99ce60360d43e118
xiaoyu <166600771+zeyu8023@users.noreply.github.com>
2025-07-05 15:06:14 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(), // ?? 不传 jsxRuntime，使用默认 classic 模式
-  ],
+  plugins: [react()],
 });
